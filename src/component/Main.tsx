943428726cac8a8c8463128e630bd996cff105d9
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-09-16 02:07:02 +0530

    main

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./style/main.scss"
+import { GrRotateLeft, GrRotateRight } from "react-icons/gr";
+import { CgMergeHorizontal, CgMergeVertical } from "react-icons/cg";
+import { IoMdUndo, IoMdRedo } from "react-icons/io";
 
 function Main() {
 
@@ -25,10 +28,10 @@ function Main() {
     ]
 
   return (
-    <div className='image_editor'>
+    <div className="image_editor">
       <div className="card">
         <div className="card_header">
-            <h2>--- Image Editor ---</h2>
+            <h2>Image Editor</h2>
         </div>
         <div className="card_body">
             <div className="sidebar">
@@ -41,6 +44,33 @@ function Main() {
                             )}
                         </div>
                     </div>
+                    <div className="filter_slider">
+                        <div className="label_bar">
+                            <label htmlFor="range">Rotate</label>
+                            <span>100%</span>
+                        </div>
+                        <input type="range" />
+                    </div>
+                    <div className="rotate">
+                        <label htmlFor="">Rotate and Flip</label>
+                        <div className="icon">
+                            <div> <GrRotateLeft/> </div>
+                            <div> <GrRotateRight/> </div>
+                            <div> <CgMergeVertical/> </div>
+                            <div> <CgMergeHorizontal/> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="image_section">
+                <div className="image"></div>
+                <div className="image_select">
+                    <button> <IoMdUndo/> </button>
+                    <button> <IoMdRedo/> </button>
+                    <button> Crop Image </button>
+                    <label htmlFor="choose">Choose Image</label>
+                    <input type="file" />
                 </div>
             </div>
         </div>
